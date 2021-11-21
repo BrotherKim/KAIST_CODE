dc79546166bc4c5386bbcf1aaad016641d00ad03
HYUNGKIM <rlagud1123@gmail.com>
2021-11-21 22:46:46 +0900

    Change run.sh args;

--- SEP531/HW_3/Homework3/run.sh
+++ SEP531/HW_3/Homework3/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = "train" ]; then
-    python3 run.py train --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep --dev-src=./data/valid.de-en.de --dev-tgt=./data/valid.de-en.en --vocab=./data/vocab.json --max_epoch=30 --cuda --save-to /content/drive/MyDrive/KAIST/SEP531/hw3_cp
+    python3 run.py train --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep --dev-src=./data/valid.de-en.de --dev-tgt=./data/valid.de-en.en --vocab=./data/vocab.json --cuda
 elif [ "$1" = "test" ]; then
-    python3 run.py decode sample_model.bin ./data/test.de-en.de ./data/test.de-en.en outputs/sample_outputs.txt
+    python3 run.py decode model.bin ./data/test.de-en.de ./data/test.de-en.en outputs/sample_outputs.txt
 elif [ "$1" = "vocab" ]; then
 	python3 vocab.py --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep ./data/vocab.json
 else
