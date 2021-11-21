b785d4551a8f12eb40e6afccc724624a94bfb8cb
HYUNGKIM <rlagud1123@gmail.com>
2021-11-21 17:55:34 +0900

    Change nmt;

--- SEP531/HW_3/Homework3/nmt.py
+++ SEP531/HW_3/Homework3/nmt.py
@@ -131,7 +131,8 @@ class NMT(nn.Module):
         
         # (source_lengths, batch_size, embed_size)
         src_word_embed = self.src_embed(source_padded)
-        packed_src_embed = pack_padded_sequence(src_word_embed, source_lengths)
+        packed_src_embed = pack_padded_sequence(src_word_embed)
+        #packed_src_embed = pack_padded_sequence(src_word_embed, source_lengths)
 
         # enc_hiddens: (source_lengths, batch_size, hidden_size)
         enc_hiddens, (last_state, last_cell) = self.encoder_lstm(packed_src_embed)
