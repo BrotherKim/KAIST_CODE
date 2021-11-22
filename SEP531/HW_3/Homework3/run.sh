61ec06623b44c28755fa00ffab22e088da191b92
HYUNGKIM <rlagud1123@gmail.com>
2021-11-22 13:33:04 +0900

    Change model.bin path;

--- SEP531/HW_3/Homework3/run.sh
+++ SEP531/HW_3/Homework3/run.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "train" ]; then
     python3 run.py train --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep --dev-src=./data/valid.de-en.de --dev-tgt=./data/valid.de-en.en --vocab=./data/vocab.json --cuda
 elif [ "$1" = "test" ]; then
-    python3 run.py decode model.bin ./data/test.de-en.de ./data/test.de-en.en outputs/sample_outputs.txt
+    python3 run.py decode /content/gdrive/MyDrive/KAIST/SEP531/hw3_cp/model.bin ./data/test.de-en.de ./data/test.de-en.en outputs/sample_outputs.txt
 elif [ "$1" = "vocab" ]; then
 	python3 vocab.py --train-src=./data/train.de-en.de.wmixerprep --train-tgt=./data/train.de-en.en.wmixerprep ./data/vocab.json
 else
